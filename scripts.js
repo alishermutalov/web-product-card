e2b22e97ea592a5a0d668b03145f2a92d40accf7
alishermutalov <96579968+alishermutalov@users.noreply.github.com>
2023-11-19 04:23:21 +0500

    changed telegram

--- scripts.js
+++ scripts.js
@@ -2,14 +2,14 @@ let tg = window.Telegram.WebApp;
 let order = document.getElementById("order");
 tg.expand();
 
-let product_name = document.getElementById('product_name');
+let product_name = document.getElementById('product_name').textContent;
 
 function buy(){
     let data = {
         product_name : product_name,
         price : 997000
     }
-
+    console.log(product_name)
     tg.sendData(JSON.stringify(data));
 
     tg.close();
